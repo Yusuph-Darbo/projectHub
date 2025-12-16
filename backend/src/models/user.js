d747ac30602ae4e8ec0705d89eeb925abf39f266
Yusuph <yusuphdarbo@gmail.com>
2025-12-16 16:01:17 +0000

    added try/catch and refactor updateUser to be dynamic

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,45 +2,77 @@ import client from '../config/db.js'
 
 export async function createUser (name, email, hashedPassword, role)
 {
-    const res = await client.query(
+    try {
+        const res = await client.query(
         'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *',
         [name, email, hashedPassword, role]
     )
 
-    return res.rows[0]
+        return res.rows[0]
+    } catch (err) {
+        console.error('Error creating user:', err);
+        throw err;
+    }
+    
 }
 
 export async function findUserByEmail (email)
 {
-    const res = await client.query(
+    try {
+        const res = await client.query(
         'SELECT * FROM users WHERE email = $1',
         [email]
     )
 
-    return res.rows[0]
+        return res.rows[0]
+    } catch (err) {
+        console.error('Error searching for user:', err);
+        throw err;
+    }
+
+    
 }
 
 export async function findUserById (user_id)
 {
-    const res = await client.query(
+    try {
+        const res = await client.query(
         'SELECT * FROM users where user_id = $1',
         [user_id]
     )
 
-    return res.rows[0]
+        return res.rows[0]
+    } catch (err) {
+        console.error('Error searching for user:', err);
+        throw err;
+    }
+    
 }
 
-export async function updateUser(user_id, name, email, role) 
+export async function updateUser(user_id, updates) 
 {
-    const res = await client.query(
+    try {
+        const keys = Object.keys(updates)
+        if (keys.length === 0) return null // Nothing to update
+
+        const setClause = keys.map((key, index) => `${key} = $${index + 1}`).join(', ')
+        const values = Object.values(updates)
+        values.push(user_id) // Add user_id for WHERE clause
+
+
+
+        const res = await client.query(
         `UPDATE users
-         SET name = $1,
-             email = $2,
-             role = $3
-         WHERE user_id = $4
+         SET ${setClause}
+         WHERE user_id = $${values.length}
          RETURNING *`,
-         [name, email, role, user_id]
+         [values]
     )
+        return res.rows[0]
     
-    return res.rows[0]
+    } catch (err) {
+        console.error('Error updating user:', err);
+        throw err;
+    }
+    
 }
