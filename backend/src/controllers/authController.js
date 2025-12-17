a1e9c7ceec13c542794367d97a87c0d3df1b159f
Yusuph <yusuphdarbo@gmail.com>
2025-12-17 16:03:30 +0000

    error checking auth

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,8 +1,10 @@
-import { createUser, getUserByEmail } from '../models/user'
+import { createUser, getUserByEmail } from '../models/user.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
 // Importing my jwt passkey from .env
-dotenv.config();
+dotenv.config()
 
 export async function register(req, res)
 {
@@ -10,14 +12,14 @@ export async function register(req, res)
     const {name, email, password} = req.body
 
     try {
-        const existingUser = getUserByEmail(email)
+        const existingUser = await getUserByEmail(email)
 
         if (existingUser){
             return res.status(400).json({ error: 'User already exists' })
         }
         
         // 10 salt rounds - Adding 10 random strings before hashing
-        const hashedPassword = await bcrypt.encrypt(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         const newUser = await createUser({ 
             name, 
@@ -47,13 +49,16 @@ export async function login (req, res)
             return res.status(400).json({ error: 'Invalid credentials' })
         }
 
-        const isMatch = await bcrypt.compare(password, hashedPassword)
+        const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch){
             return res.status(400).json({ error: 'Invalid credentials' })
         }
 
-        // Create a JWT token containing the user's ID, signed with the server secret
-        // Will expire in an 1 hour and can be used by the client to authenticate future requests
+        /* Create a JWT token containing the user's ID, signed with the server secret
+           Will expire in an 1 hour and can be used by the client to authenticate future requests
+           Authenticates and authorises users which allows users to create tasks only if they
+           are logged in / view projects they are apart of
+        */
         const token = jwt.sign({ id: user.id}, process.env.JWT_SECRET, { expiresIn: '1h'})
 
         res.json({ token, user: { id: user.id, name: user.name, email: user.email } })
