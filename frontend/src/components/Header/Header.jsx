9627bc20b9736077e911507348430f49bc8987fc
Yusuph <yusuphdarbo@gmail.com>
2025-12-30 18:03:43 +0000

    frame of header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+export default function Header (props) {
+    return (
+        <header>
+            <div className="left">
+                <div>
+                    <h1>{props.title}</h1>
+                    {props.subtitle && <p>{props.subtitle}</p>}
+                </div>
+            </div>
+
+            <div className="right">
+                {props.action}
+            </div>
+        </header>
+    )
+}
