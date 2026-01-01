43b0b7c6e49fa675719a6119b750120b3107acad
Yusuph <yusuphdarbo@gmail.com>
2026-01-01 18:25:30 +0000

    implemented router in app & main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./styles/global.css";
+
 import Header from "./components/Header/Header.jsx";
 import Register from "./pages/Register/Register.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Kanban from "./pages/Kanban/Kanban.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 export default function App() {
   return (
@@ -15,9 +17,12 @@ export default function App() {
         // showBack
         action={<button>+ Add Task</button>}
       />
-      {/* <Register /> */}
-      {/* <Home /> */}
-      {/* <Kanban /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Kanban />} />
+      </Routes>
+
       <Footer />
     </>
   );
