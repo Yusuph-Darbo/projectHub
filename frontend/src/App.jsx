fd310a5b7efe3154c0a1ecb1d6a4e0aed6de0e45
Yusuph <yusuphdarbo@gmail.com>
2026-01-01 14:22:11 +0000

    initialised kanban page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/global.css";
 import Header from "./components/Header/Header.jsx";
 import Register from "./pages/Register/Register.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 export default function App() {
   return (
@@ -10,9 +11,10 @@ export default function App() {
         title="ProjectHub"
         subtitle="Track and manage your project tasks"
         // showBack
-        // action={<button>+ Add Task</button>}
+        action={<button>+ Add Task</button>}
       />
-      <Register />
+      {/* <Register /> */}
+      <Home />
       <Footer />
     </>
   );
