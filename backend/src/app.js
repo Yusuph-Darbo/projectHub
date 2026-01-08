fe70f69a5a481ee6852854f77bda89b9b07f3ec0
Yusuph <yusuphdarbo@gmail.com>
2026-01-08 19:13:14 +0000

    fixed error on login & register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,31 @@
-import express from "express"
-import authRoutes from './routes/authRoute.js'
-import projectRoutes from './routes/projectRoute.js'
-import taskRoutes from './routes/taskRoute.js'
-import taskAssignmentRoutes from './routes/taskAssignmentRoute.js'
-import dotenv from 'dotenv'
+import express from "express";
+import authRoutes from "./routes/authRoute.js";
+import projectRoutes from "./routes/projectRoute.js";
+import taskRoutes from "./routes/taskRoute.js";
+import taskAssignmentRoutes from "./routes/taskAssignmentRoute.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
 // Importing my jwt passkey from .env
 // Have to run the app from backend folder due to relative pathing bruhh
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/project', projectRoutes)
-app.use('/api/task', taskRoutes)
-app.use('/api/task-assignment', taskAssignmentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/task", taskRoutes);
+app.use("/api/task-assignment", taskAssignmentRoutes);
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
