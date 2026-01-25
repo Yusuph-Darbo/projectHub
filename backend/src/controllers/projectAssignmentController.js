0fa66ea9579ccd8f9a7802cdcc07b24dbdb7c8a8
Yusuph <yusuphdarbo@gmail.com>
2026-01-25 19:40:14 +0000

    route to unassign user from project

--- backend/src/controllers/projectAssignmentController.js
+++ backend/src/controllers/projectAssignmentController.js
@@ -21,3 +21,22 @@ export async function addUserToProjectController(req, res) {
     res.status(500).json({ error: "Assigning user to project" });
   }
 }
+
+export async function removeUserFromProjectController(req, res) {
+  try {
+    const project_id = req.params.id;
+    const { user_id } = req.body;
+
+    if (!user_id || !user_id) {
+      return res
+        .status(400)
+        .json({ error: "user_id and task_id are required" });
+    }
+
+    const unassignment = await removeUserFromProject(user_id, project_id);
+
+    res.status(200).json(unassignment);
+  } catch (err) {
+    res.status(500).json({ error: "Unassigning user from project" });
+  }
+}
--- backend/src/routes/projectAssignmentRoute.js
+++ backend/src/routes/projectAssignmentRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { requireAuth } from "../middleware/auth.js";
-import { addUserToProjectController } from "../controllers/projectAssignmentController.js";
+import {
+  addUserToProjectController,
+  removeUserFromProjectController,
+} from "../controllers/projectAssignmentController.js";
 
 const router = express.Router();
 
 router.post("/:id/assign", requireAuth, addUserToProjectController);
+router.delete("/:id/unassign", requireAuth, removeUserFromProjectController);
 
 export default router;
