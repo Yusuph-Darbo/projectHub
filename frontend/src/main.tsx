5a0ed185c9a9a799143dbfb88fbda35f8d5d52f0
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-10 14:55:34 +0000

    converted app and main to ts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/global.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
