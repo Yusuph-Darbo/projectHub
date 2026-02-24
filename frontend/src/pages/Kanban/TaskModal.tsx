810ce1fb332b69aaffeca1fe398fb580036f849f
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-24 15:12:15 +0000

    initialised import and typing for taskModal

---
+++ frontend/src/pages/Kanban/TaskModal.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card.jsx";
+import { FaPlus } from "react-icons/fa";
+import type { EnrichedTask } from "../../types/task.js";
+import type { Member } from "../../types/projectAssignment.js";
+
+interface TaskModalProps {
+  mode: "create" | "edit";
+  task?: EnrichedTask | null;
+  members: Member[];
+  title: string;
+  setTitle: (v: string) => void;
+  description: string;
+  setDescription: (v: string) => void;
+  status: string;
+  setStatus: (v: string) => void;
+  assignee: string;
+  setAssignee: (v: string) => void;
+  onSave: () => void;
+  onDelete?: () => void;
+  onClose: () => void;
+  isLoading: boolean;
+}
