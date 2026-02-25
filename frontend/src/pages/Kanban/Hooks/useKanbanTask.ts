53f0a931fdf530c70f95deb5b9528a83c3982a6c
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-25 11:30:14 +0000

    useStates for kanban tasks

--- frontend/src/pages/Kanban/Hooks/useKanbanTask.ts
+++ frontend/src/pages/Kanban/Hooks/useKanbanTask.ts
@@ -17,3 +17,26 @@ import type { Task, EnrichedTask } from "../../../types/task.js";
 import type { ProjectOwner } from "../../../types/project.js";
 import type { Member } from "../../../types/projectAssignment.js";
 import { toast } from "sonner";
+
+interface column {
+  id: string;
+  title: string;
+  bgColor: string;
+  borderColor: string;
+  statusColor: string;
+  statusTextColor: string;
+  tasks: EnrichedTask[];
+  count: number;
+}
+
+export default function useKanbanTasks(projectId: string) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [members, setMembers] = useState<Member[]>([]);
+  const [projectOwner, setProjectOwner] = useState<ProjectOwner | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const currentUser = getCurrentUser();
+
+  const isOwner =
+    currentUser && projectOwner && currentUser.id === projectOwner.user_id;
+}
