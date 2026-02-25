8c112577012804f6d97d07685ce4b274796fe5bd
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-02-25 11:39:22 +0000

    userMap and column logic

--- frontend/src/pages/Kanban/Hooks/useKanbanTask.ts
+++ frontend/src/pages/Kanban/Hooks/useKanbanTask.ts
@@ -18,7 +18,7 @@ import type { ProjectOwner } from "../../../types/project.js";
 import type { Member } from "../../../types/projectAssignment.js";
 import { toast } from "sonner";
 
-interface column {
+interface Column {
   id: string;
   title: string;
   bgColor: string;
@@ -77,4 +77,84 @@ export default function useKanbanTasks(projectId: string) {
     }
     fetchOwner();
   }, [projectId]);
+
+  // UserMap
+  // cache the result of a calculation between re-renders.
+  const userMap = useMemo(() => {
+    const map: Record<number, string> = {};
+
+    // Creating a dictionary / object between user_id and names
+    members.forEach((m) => {
+      map[m.user_id] = m.name;
+    });
+
+    if (projectOwner) {
+      map[projectOwner.user_id] = projectOwner.name;
+    }
+
+    return map;
+  }, [members, projectOwner]);
+
+  // Column Config
+  const columnConfig = {
+    "To Do": {
+      id: "todo",
+      title: "To Do",
+      bgColor: "#F9FAFC",
+      borderColor: "#CAD5E2",
+      statusColor: "#F1F5F9",
+      statusTextColor: "#324158",
+    },
+    "In Progress": {
+      id: "in-progress",
+      title: "In Progress",
+      bgColor: "#EEF6FF",
+      borderColor: "#8EC5FF",
+      statusColor: "#DBEAFF",
+      statusTextColor: "#1447E5",
+    },
+    Done: {
+      id: "done",
+      title: "Done",
+      bgColor: "#F0FDF4",
+      borderColor: "#7AF1A8",
+      statusColor: "#DCFCE6",
+      statusTextColor: "#008236",
+    },
+  };
+
+  const columns: Column[] = useMemo(() => {
+    const cols: Column[] = Object.values(columnConfig).map((col) => ({
+      ...col,
+      tasks: [],
+      count: 0,
+    }));
+
+    tasks.forEach((task) => {
+      const column = cols.find((c) => c.title === task.status);
+      if (!column) return;
+
+      column.tasks.push({
+        // Preserve original fields so we keep IDs
+        id: task.task_id,
+        title: task.title,
+        description: task.description,
+        status: task.status,
+        created_by: task.created_by,
+        assigned_to: task.assigned_to ?? null,
+        // Derived display fields
+        createdByLabel: userMap[task.created_by] ?? "Unknown user",
+        assignedToLabel:
+          task.assigned_to != null
+            ? (userMap[task.assigned_to] ?? "Unknown user")
+            : "Unassigned",
+        statusColor: column.statusColor,
+        statusTextColor: column.statusTextColor,
+      });
+
+      column.count++;
+    });
+
+    return cols;
+  }, [tasks, userMap]);
 }
